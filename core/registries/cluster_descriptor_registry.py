1bd6ec1dfe4f29c7ad9ee45228d49ece4d510e6d
vladstudennikov <studennikovvlad@gmail.com>
2026-06-29 14:49:49 +0300

    Update cluster descriptors registry

--- core/registries/cluster_descriptor_registry.py
+++ core/registries/cluster_descriptor_registry.py
@@ -3,5 +3,5 @@ from core.registries.component_registry import ComponentRegistry
 CLUSTER_DESCRIPTORS = ComponentRegistry("cluster_descriptor")
 CLUSTER_DESCRIPTORS.register(
     "ollama",
-    "cluster_descriptor_agent.agent:ClusterDescriptorAgent",
+    "cluster_descriptor_agent.agent:OllamaClusterDescriptorAgent",
 )
